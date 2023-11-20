b6d85b47b12ee736f2447a4cce279be104b73faa
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-11-20 12:08:11 +0600

    footer setup root

--- src/Layout/RootLayout.jsx
+++ src/Layout/RootLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../Header/NavBar";
+import Footer from "../Footer/Footer";
 
 
 const RootLayout = () => {
@@ -9,6 +10,7 @@ const RootLayout = () => {
             <div>
                 <Outlet />
             </div>
+            <Footer />
         </div>
     );
 };
