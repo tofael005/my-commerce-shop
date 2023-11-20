d8638a873c980eaef667051be6db09ce6bcd2953
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-11-21 00:57:39 +0600

    banner nav

--- src/Header/Banner.jsx
+++ src/Header/Banner.jsx
@@ -9,23 +9,54 @@ import slider2 from "../assets/slider/slider2.jpg"
 
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
+import { IoIosArrowForward } from "react-icons/io";
+
 const Banner = () => {
     return (
         <div className='flex justify-between flex-col md:flex-row mt-3 items-center gap-6 max-w-[1240px] mx-auto'>
 
             <div className='md:w-[400px]'>
-                <ul>
-                    <li>Smart Watch</li>
-                    <li>Smart Phone</li>
-                    <li>Laptop</li>
-                    <li>Tablet</li>
-                    <li>Monitor</li>
-                    <li></li>
-                    <li>Smart Watch</li>
-                    <li>Smart Watch</li>
-                    <li>Smart Watch</li>
-                    <li>Smart Watch</li>
-                </ul>
+
+                <div>
+                    <h1> Smart Watch</h1>
+                    <IoIosArrowForward />
+                </div>
+                <div>
+                    <h1>Smart Phone</h1>
+                    <IoIosArrowForward />
+                </div>
+                <div>
+                    <h1>Laptop</h1>
+                    <IoIosArrowForward />
+                </div>
+                <div>
+                    <h1> Tablet</h1>
+                    <IoIosArrowForward />
+                </div>
+                <div>
+                    <h1>Monitor</h1>
+                    <IoIosArrowForward />
+                </div>
+                <div>
+                    <h1>Printer</h1>
+                    <IoIosArrowForward />
+                </div>
+                <div>
+                    <h1>Printer</h1>
+                    <IoIosArrowForward />
+                </div>
+                <div>
+                    <h1>Gamming Box</h1>
+                    <IoIosArrowForward />
+                </div>
+                <div>
+                    <h1>Software</h1>
+                    <IoIosArrowForward />
+                </div>
+                <div>
+                    <h1>Software</h1>
+                    <IoIosArrowForward />
+                </div>
             </div>
 
 
