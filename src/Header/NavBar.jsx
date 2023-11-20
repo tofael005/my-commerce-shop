7dbf87ec7d1a11f247a739cbdafd7006918cbf41
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-11-20 20:30:31 +0600

    nab header

--- src/Header/NavBar.jsx
+++ src/Header/NavBar.jsx
@@ -3,6 +3,12 @@
 const NavBar = () => {
     return (
         <div>
+            <div>
+                <h1>Welcome to worldwide Megamart!</h1>
+                <div>
+                    <h1></h1>
+                </div>
+            </div>
             <h1>Navbar</h1>
         </div>
     );
