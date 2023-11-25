75ef0c95248cc011a2c4eccb403e3b7b9bac18c8
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-11-26 00:16:59 +0600

    view product added

--- src/Components/Apple/Viewproduct.jsx
+++ src/Components/Apple/Viewproduct.jsx
@@ -1,9 +1,15 @@
-
+import Image from "../../assets/Phone Brand/14-pro-max.webp"
 
 const Viewproduct = () => {
     return (
         <div className="max-w-[1240px] mx-auto mt-10 px-2">
             <h1>Visi product details</h1>
+            <div className="flex flex-col md:flex-row gap-4">
+                <img className="w-[600px]" src={Image} alt="" />
+                <div>
+                    <h1>Iphone 14 Pro Max</h1>
+                </div>
+            </div>
         </div>
     );
 };
