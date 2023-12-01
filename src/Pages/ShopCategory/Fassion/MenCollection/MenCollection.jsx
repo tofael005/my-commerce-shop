875c3647ecb1bf23e3aa848719e391e2fc536a10
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-12-01 14:51:40 +0600

    hsf

--- src/Pages/ShopCategory/Fassion/MenCollection/MenCollection.jsx
+++ src/Pages/ShopCategory/Fassion/MenCollection/MenCollection.jsx
@@ -4,7 +4,7 @@ const MenCollection = () => {
     return (
         <div className="max-w-[1240px] mx-auto mt-10 mb-10 px-2">
             <h1 className="text-center text-6xl font-bold mt-10 mb-3">Mens Collection</h1>
-            <p className="text-center text-xl">Comming SOON!</p>
+            <p className="text-center text-xl">Comming SOON!.</p>
         </div>
     );
 };
