1f684c3440e8de735b941773955e94f0db3b1a45
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-12-01 18:28:52 +0600

    ens stat

--- src/Pages/ShopCategory/Fassion/MenCollection/MenCollection.jsx
+++ src/Pages/ShopCategory/Fassion/MenCollection/MenCollection.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 
 
 const MenCollection = () => {
+    const [mens, setMens] = useState([])
+
+
+    
     return (
         <div className="max-w-[1240px] mx-auto mt-10 mb-10 px-2">
             <h1 className="text-center text-6xl font-bold mt-10 mb-3">Mens Collection</h1>
