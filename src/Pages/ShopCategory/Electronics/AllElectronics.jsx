b0a80346058a844c6ca1c8100fb556e743e0d573
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-12-06 12:15:15 +0600

    Soon

--- src/Pages/ShopCategory/Electronics/AllElectronics.jsx
+++ src/Pages/ShopCategory/Electronics/AllElectronics.jsx
@@ -4,7 +4,7 @@ const AllElectronics = () => {
     return (
         <div className="max-w-[1240px] mx-auto mt-10 px-2">
 
-            <p className="text-5xl text-center font-bold mb-3">Comming SOON</p>
+            <p className="text-5xl text-center font-bold mb-3">Comming SOON!</p>
             <h1 className="text-center font-semibold">All Electronicss</h1>
 
         </div>
