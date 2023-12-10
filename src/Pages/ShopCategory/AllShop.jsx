0dbbe16d64dd78a6e3253ce89bdb18b5160d6cbe
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-12-10 21:47:37 +0600

    lo

--- src/Pages/ShopCategory/AllShop.jsx
+++ src/Pages/ShopCategory/AllShop.jsx
@@ -29,7 +29,7 @@ const AllShop = () => {
                     <div className="p-4 bg-slate-100 rounded-md">
                         <img className="rounded-full border block mx-auto w-[100px] h-[100px]" src={shop1} alt="" />
                     </div>
-                    <Link to="/fashion"><h1 className="text-center  hover:bg-[#008ECC] hover:text-white duration-300 rounded-b-md p-2">Life Syle & Fashion</h1></Link>
+                    <Link to="/fashion"><h1 className="text-center  hover:bg-[#008ECC] hover:text-white duration-300 rounded-b-md p-2">Life Syle & Fashion.</h1></Link>
                 </div>
                 <div className="shadow rounded-md">
                     <div className="p-4 bg-slate-100 rounded-md">
