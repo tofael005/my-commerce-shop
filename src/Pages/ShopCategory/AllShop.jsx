9b5daab14ae12f59005c98dfa38f43c1de713350
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-12-13 23:48:41 +0600

    asors

--- src/Pages/ShopCategory/AllShop.jsx
+++ src/Pages/ShopCategory/AllShop.jsx
@@ -12,10 +12,10 @@ const AllShop = () => {
     return (
         <div className="max-w-[1240px] mx-auto px-2 mb-16">
             <div className="flex justify-between items-center my-3">
-                <h1>Shop From <span className="text-[#008ECC] font-bold">Top Categories..</span></h1>
+                <h1>Shop From <span className="text-[#008ECC] font-bold">Top Categories</span></h1>
                 <Link to="/multiple-shop">
                     <div className="flex gap-2 items-center cursor-pointer hover:bg-[#008ECC] duration-300 hover:text-white px-3 rounded">
-                        <h1>View All..</h1>
+                        <h1>View All</h1>
                         <IoIosArrowForward />
                     </div>
                 </Link>
@@ -29,13 +29,13 @@ const AllShop = () => {
                     <div className="p-4 bg-slate-100 rounded-md">
                         <img className="rounded-full border block mx-auto w-[100px] h-[100px]" src={shop1} alt="" />
                     </div>
-                    <Link to="/fashion"><h1 className="text-center  hover:bg-[#008ECC] hover:text-white duration-300 rounded-b-md p-2">Life Syle & Fashion..</h1></Link>
+                    <Link to="/fashion"><h1 className="text-center  hover:bg-[#008ECC] hover:text-white duration-300 rounded-b-md p-2">Life Syle & Fashion</h1></Link>
                 </div>
                 <div className="shadow rounded-md">
                     <div className="p-4 bg-slate-100 rounded-md">
                         <img className="rounded-full border block mx-auto w-[100px] h-[100px]" src={shop6} alt="" />
                     </div>
-                    <Link to="/allPhone"><h1 className="text-center  hover:bg-[#008ECC] hover:text-white duration-300 rounded-b-md p-2">Gadghet & Device</h1></Link>
+                    <Link to="/allPhone"><h1 className="text-center  hover:bg-[#008ECC] hover:text-white duration-300 rounded-b-md p-2">Gadghet & Device.</h1></Link>
                 </div>
                 <div className="shadow rounded-md">
                     <div className="p-4 bg-slate-100 rounded-md">
