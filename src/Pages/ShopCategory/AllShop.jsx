8bef35dc7f59178ac2d184b03e22a80e34923e0b
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-12-15 21:12:37 +0600

    frozz

--- src/Pages/ShopCategory/AllShop.jsx
+++ src/Pages/ShopCategory/AllShop.jsx
@@ -59,13 +59,13 @@ const AllShop = () => {
                     <div className="p-4 bg-slate-100 rounded-md">
                         <img className="rounded-full border block mx-auto w-[100px] h-[100px]" src={shop4} alt="" />
                     </div>
-                    <Link to="/frozen"><h1 className="text-center  hover:bg-[#008ECC] hover:text-white duration-300 rounded-b-md p-2">Frozen Food</h1></Link>
+                    <Link to="/frozen"><h1 className="text-center  hover:bg-[#008ECC] hover:text-white duration-300 rounded-b-md p-2">Frozen Food.</h1></Link>
                 </div>
                 <div className="shadow rounded-md">
                     <div className="p-4 bg-slate-100 rounded-md">
                         <img className="rounded-full border block mx-auto w-[100px] h-[100px]" src={shop5} alt="" />
                     </div>
-                  <Link to="/grocery"><h1 className="text-center  hover:bg-[#008ECC] hover:text-white duration-300 rounded-b-md p-2">Grocery Items..</h1></Link>
+                  <Link to="/grocery"><h1 className="text-center  hover:bg-[#008ECC] hover:text-white duration-300 rounded-b-md p-2">Grocery Items</h1></Link>
                 </div>
             </div>
         </div>
