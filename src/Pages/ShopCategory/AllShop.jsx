cf042306252d47d0a7dcac02b953259e6266946a
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-12-17 22:20:09 +0600

    grocerys

--- src/Pages/ShopCategory/AllShop.jsx
+++ src/Pages/ShopCategory/AllShop.jsx
@@ -41,7 +41,7 @@ const AllShop = () => {
                     <div className="p-4 bg-slate-100 rounded-md">
                         <img className="rounded-full border block mx-auto w-[100px] h-[100px]" src={shop7} alt="" />
                     </div>
-                    <Link to="/electronics"><h1 className="text-center  hover:bg-[#008ECC] hover:text-white duration-300 rounded-b-md p-2">Electronics...</h1></Link>
+                    <Link to="/electronics"><h1 className="text-center  hover:bg-[#008ECC] hover:text-white duration-300 rounded-b-md p-2">Electronics..</h1></Link>
                 </div>
                 <div className="shadow rounded-md">
                     <div className="p-4 bg-slate-100 rounded-md">
@@ -65,7 +65,7 @@ const AllShop = () => {
                     <div className="p-4 bg-slate-100 rounded-md">
                         <img className="rounded-full border block mx-auto w-[100px] h-[100px]" src={shop5} alt="" />
                     </div>
-                  <Link to="/grocery"><h1 className="text-center  hover:bg-[#008ECC] hover:text-white duration-300 rounded-b-md p-2">Grocery Items.</h1></Link>
+                  <Link to="/grocery"><h1 className="text-center  hover:bg-[#008ECC] hover:text-white duration-300 rounded-b-md p-2">Grocery Items</h1></Link>
                 </div>
             </div>
         </div>
