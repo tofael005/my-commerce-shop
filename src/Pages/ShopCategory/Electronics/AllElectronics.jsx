ebb7a7931874dfc7926fd098fdb068945fc1c29f
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-12-18 23:37:37 +0600

    alls

--- src/Pages/ShopCategory/Electronics/AllElectronics.jsx
+++ src/Pages/ShopCategory/Electronics/AllElectronics.jsx
@@ -5,7 +5,7 @@ const AllElectronics = () => {
         <div className="max-w-[1240px] mx-auto mt-10 px-2">
 
             <p className="text-5xl text-center font-bold mb-3">Comming SOON!</p>
-            <h1 className="text-center font-semibold">All Electronicss</h1>
+            <h1 className="text-center font-semibold">All Electronics</h1>
 
         </div>
     );
