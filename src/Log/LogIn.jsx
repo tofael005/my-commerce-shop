b47b8dd90656f9583db78f2c77ae3f8c8150fc11
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2023-12-19 22:14:26 +0600

    updat

--- src/Log/LogIn.jsx
+++ src/Log/LogIn.jsx
@@ -21,7 +21,7 @@ const LogIn = () => {
                                 <label className="text-xl text-white font-semibold" htmlFor="">Password: </label><br />
                                 <input className="md:w-[500px] mt-1 mb-2 w-[350px] p-3 border rounded-md bg-transparent " type="password" name="password" id="" placeholder="Enter your password" /><br />
                                 <input className="md:w-[500px] font-bold w-[350px] block mx-auto mt-4 bg-slate-200 p-3 rounded-md text-sky-500 cur hover:bg-sky-400 hover:text-white duration-300 cursor-pointer" type="button" value="Sign In" />
-                                <p className="text-center mt-3 text-white ">You don't have an account? <span className="text-sky-400 font-semibold"><Link to="/registration">Sign Up</Link></span></p>
+                                <p className="text-center mt-3 text-white ">You dont have an account? <span className="text-sky-400 font-semibold"><Link to="/registration">Sign Up</Link></span></p>
                             </form>
                         </div>
 
